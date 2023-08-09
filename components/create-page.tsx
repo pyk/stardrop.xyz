99f320023538f8861ed07c2a7a6736b3eaf92b5e
pyk <gm@pyk.sh>
2023-08-10 06:30:13 +0700

    chore: add network selection

--- components/create-page.tsx
+++ components/create-page.tsx
@@ -3,11 +3,190 @@
 import { useSIWE } from "connectkit";
 import { SignInButton } from "@/components/sign-in-button";
 
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  RadioGroup,
+  RadioGroupItem,
+  RadioGroupChainItem,
+} from "./ui/radio-group";
+import { Icons } from "@/components/icons";
+
+const CreateFormSchema = z.object({
+  chainId: z
+    .enum(["1", "2", "3"], {
+      required_error: "You need to select a network.",
+    })
+    .transform((c) => Number(c)),
+  name: z.string().min(2).max(50),
+  symbol: z.string().toUpperCase().min(3).max(15),
+});
+
 export function CreatePage() {
   const siwe = useSIWE();
 
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof CreateFormSchema>>({
+    resolver: zodResolver(CreateFormSchema),
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof CreateFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   if (siwe.isSignedIn) {
-    return <div>signed in</div>;
+    return (
+      <div className="p-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            {/* Form header */}
+            <div className="flex flex-col space-y-2">
+              <h1 className="font-bold text-3xl text-gray-900">
+                Create Stardrop
+              </h1>
+              <p className="font-medium text-gray-500 text-lg">
+                Reward onchain activity with special NFT
+              </p>
+            </div>
+
+            {/* Onchain action group */}
+            <div className="mt-8">
+              <h2 className="font-bold text-lg text-gray-900">
+                Onchain action
+              </h2>
+              <p className="text-gray-500">
+                Specify required onchain action to claim the NFT
+              </p>
+            </div>
+
+            {/* Select source chain */}
+            <div className="mt-8">
+              <FormField
+                control={form.control}
+                name="chainId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-bold text-gray-900">
+                      Choose network
+                    </FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={`${field.value}`}
+                        className="grid grid-cols-3 gap-2"
+                      >
+                        {/* Ethereum */}
+                        <FormItem className="space-y-0">
+                          <FormControl>
+                            <RadioGroupChainItem value="1">
+                              <div className="flex flex-col space-y-2 items-center">
+                                <Icons.ethereum />
+                                <div className="flex flex-col space-y-1">
+                                  <h3 className="text-sm font-bold text-gray-900 leading-none">
+                                    Ethereum
+                                  </h3>
+                                  <p className="text-sm font-medium text-gray-500 leading-none">
+                                    ETH
+                                  </p>
+                                </div>
+                              </div>
+                            </RadioGroupChainItem>
+                          </FormControl>
+                        </FormItem>
+
+                        {/* Optimism */}
+                        <FormItem>
+                          <FormControl>
+                            <RadioGroupChainItem value="op">
+                              <div className="flex flex-col space-y-2 items-center">
+                                <Icons.optimism />
+                                <div className="flex flex-col space-y-1">
+                                  <h3 className="text-sm font-bold text-gray-900 leading-none">
+                                    Optimism
+                                  </h3>
+                                  <p className="text-sm font-medium text-gray-500 leading-none">
+                                    OP
+                                  </p>
+                                </div>
+                              </div>
+                            </RadioGroupChainItem>
+                          </FormControl>
+                        </FormItem>
+
+                        {/* Base */}
+                        <FormItem>
+                          <FormControl>
+                            <RadioGroupChainItem value="base">
+                              <div className="flex flex-col space-y-2 items-center">
+                                <Icons.base />
+                                <div className="flex flex-col space-y-1">
+                                  <h3 className="text-sm font-bold text-gray-900 leading-none">
+                                    Base
+                                  </h3>
+                                  <p className="text-sm font-medium text-gray-500 leading-none">
+                                    BASE
+                                  </p>
+                                </div>
+                              </div>
+                            </RadioGroupChainItem>
+                          </FormControl>
+                        </FormItem>
+
+                        {/* Zora */}
+                        <FormItem>
+                          <FormControl>
+                            <RadioGroupChainItem value="zora">
+                              <div className="flex flex-col space-y-2 items-center">
+                                <Icons.zora />
+                                <div className="flex flex-col space-y-1">
+                                  <h3 className="text-sm font-bold text-gray-900 leading-none">
+                                    ZORA
+                                  </h3>
+                                  <p className="text-sm font-medium text-gray-500 leading-none">
+                                    ZORB
+                                  </p>
+                                </div>
+                              </div>
+                            </RadioGroupChainItem>
+                          </FormControl>
+                        </FormItem>
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Onchain action group */}
+            <div className="mt-8">
+              <h2 className="font-bold text-lg text-gray-900">Reward</h2>
+              <p className="text-gray-500">NFT for the eligible users</p>
+            </div>
+          </form>
+        </Form>
+      </div>
+    );
   } else {
     // flex-1 with parent flex and flex-col will fill remaining height
     return (
@@ -17,7 +196,7 @@ export function CreatePage() {
             Create on Stardrop
           </h1>
           <p className=" text-xl max-w-[320px] sm:max-w-none text-center text-gray-500">
-            Start creating on Stardrop by connecting your wallet first.
+            Start creating on Stardrop by connecting your wallet first
           </p>
         </div>
         <div>
---
+++ components/ui/radio-group.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import * as React from "react";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
+import { Circle } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+
+const RadioGroup = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Root
+      className={cn("grid gap-2", className)}
+      {...props}
+      ref={ref}
+    />
+  );
+});
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
+
+const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        "aspect-square h-4 w-4 rounded-full border border-slate-200 border-slate-900 text-slate-900 ring-offset-white focus:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:border-slate-50 dark:text-slate-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+        className
+      )}
+      {...props}
+    >
+      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+        <Circle className="h-2.5 w-2.5 fill-current text-current" />
+      </RadioGroupPrimitive.Indicator>
+    </RadioGroupPrimitive.Item>
+  );
+});
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
+
+const RadioGroupChainItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        "border border-gray-200 rounded-2xl px-4 py-6 data-[state=checked]:bg-gray-100 data-[state=checked]:border-gray-900 min-w-[100px] w-full",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </RadioGroupPrimitive.Item>
+  );
+});
+RadioGroupChainItem.displayName = RadioGroupPrimitive.Item.displayName;
+
+export { RadioGroup, RadioGroupItem, RadioGroupChainItem };
